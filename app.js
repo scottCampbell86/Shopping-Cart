24d8dfcc5fac7eee06ed9e38c95180b0ebe97d78
Scott Campbell <stc9486@gmail.com>
2020-01-21 16:51:03 -0800

    static

---
+++ app.js
@@ -0,0 +1,13 @@
+/*const heading = document.getElementById('heading');
+const title = document.getElementById('title');
+//const image = image...
+const description = document.getElementById('description');
+const year = document.getElementById('year');
+const label = document.getElementById('label');
+const popularSong = documeny.getElementById(popular-song);*/
+
+
+
+
+
+
